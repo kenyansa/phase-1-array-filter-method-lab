07b19786a8f03d34b3acbe307564fd659cb04e4e
Kenyansa <kenyansajustine@gmail.com>
2022-04-13 07:38:24 +0300

    introduce necessary comments for solving the test stepwise

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+//  findMatching(): returns all drivers that match the passed in name:
+    
+// findMatching(): returns matching drivers if case does not match but letters do:
+  
+// findMatching(): returns an empty array if there is no match:
+  
+// fuzzyMatch(): returns a driver if beginning provided letters match:
+    
+// fuzzyMatch(): does not return drivers if only middle or ending letters match:
+    
+// fuzzyMatch():does not return drivers if only middle or ending letters match:
+   
+// matchName(): accesses the data structure to check if name matches:
